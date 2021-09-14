03fd791109159531caeb9e0b17e1b526b71b8295
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-09-14 16:38:12 +0100

    add cis foundation module

---
+++ cis_foundation/variables.tf
@@ -0,0 +1,19 @@
+variable "cis_metric_namespace" {
+  type        = string
+  default     = "CISLogMetrics"
+  description = "The destination namespace of the CIS CloudWatch metric."
+}
+
+variable "account_name" {
+  type = string
+}
+
+variable "aws_cloudwatch_log_group_cloudtrail_name" {
+  type        = string
+  description = "Name of the trail."
+}
+
+variable "tags" {
+  description = "A map of tags to assign to resources."
+  type        = map(string)
+}
