568f39e9ceb6aa6517abdc6a0423ff0d87ed2150
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-09-15 10:00:21 +0100

    restore variable

--- cis_foundation.tf
+++ cis_foundation.tf
@@ -1,4 +1,5 @@
 module "cis_foundation" {
+  count                                    = var.cis_foundation_alarms_enabled == true ? 1 : 0
   source                                   = "./cis_foundation"
   account_name                             = var.account_name
   cis_metric_namespace                     = var.cis_metric_namespace
