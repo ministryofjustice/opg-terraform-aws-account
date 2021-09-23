369c635e9ada605c4cd3edf7fe27cedc24b876b0
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-09-23 14:46:28 +0100

    create dependency between sh and sh controls

--- security_hub/cis_foundation_control_status.tf
+++ security_hub/cis_foundation_control_status.tf
@@ -6,4 +6,7 @@ resource "aws_securityhub_standards_control" "ensure_hardware_mfa_is_enabled_for
   standards_control_arn = "${local.cis_standard_controls_arn_path}/1.14"
   control_status        = var.cis_1_14_control_status
   disabled_reason       = var.cis_1_14_disabled_reason
+  depends_on = [
+    aws_securityhub_account.main
+  ]
 }
