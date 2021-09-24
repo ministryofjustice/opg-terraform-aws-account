fb186b7f536488a57d73860921c74e941571ef4d
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-09-24 14:28:02 +0100

    fix typo in input variable

--- moj_security_baseline.tf
+++ moj_security_baseline.tf
@@ -13,7 +13,7 @@ module "security_hub" {
   product                                  = var.product
   cis_metric_namespace                     = var.cis_metric_namespace
   aws_cloudwatch_log_group_cloudtrail_name = var.cloudtrail_trail_name
-  cis_foundation_control_1_14_enabled      = var.cis_foundation_control_3_4_enabled
+  cis_foundation_control_1_14_enabled      = var.cis_foundation_control_1_14_enabled
   cis_foundation_control_3_4_enabled       = var.cis_foundation_control_3_4_enabled
   cis_foundation_control_3_8_enabled       = var.cis_foundation_control_3_8_enabled
   cis_foundation_control_3_10_enabled      = var.cis_foundation_control_3_10_enabled
