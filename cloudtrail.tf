1c7b55db74d8a9dcf01c848efece9432a2797660
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-01-06 12:28:57 +0000

    no longer supply tags in favour of default tags on the provider

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -3,5 +3,4 @@ module "cloudtrail" {
   trail_name                    = var.cloudtrail_trail_name
   bucket_name                   = var.cloudtrail_bucket_name
   s3_access_logging_bucket_name = aws_s3_bucket.s3_access_logging.id
-  tags                          = local.mandatory_moj_tags
 }
