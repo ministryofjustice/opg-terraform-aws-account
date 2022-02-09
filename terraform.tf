7dda906c73fffcc7bfc0177fef27af08ba5b0510
Aidan Johnson <aidan.johnson@digital.justice.gov.uk>
2022-02-09 14:26:13 +0000

    SP-980 - Set required terraform version

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,14 @@
 terraform {
-    required_providers {
-        aws = {
-        source  = "hashicorp/aws"
-        version = ">= 3.59.0"
-        configuration_aliases = [
-            aws.eu-west-2,
-        ]
-        }
+  required_version = ">= 1.1.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.59.0"
+      configuration_aliases = [
+        aws.eu-west-2,
+      ]
     }
+  }
 }
 
 data "aws_region" "current" {}
