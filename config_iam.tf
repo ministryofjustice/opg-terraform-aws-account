b9dffeb45edcc21db39c5ba01b9357b72b703c53
Aidan Johnson <aidan.johnson@digital.justice.gov.uk>
2022-02-09 14:24:24 +0000

    SP-980 - Only create config user if config enabled

--- config_iam.tf
+++ config_iam.tf
@@ -1,4 +1,5 @@
 resource "aws_iam_role" "config" {
+  count              = var.baseline_security_enabled ? 1 : 0
   name               = "aws-config-${var.product}-${var.account_name}"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
@@ -18,6 +19,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "managed_policy" {
-  role       = aws_iam_role.config.name
+  count      = var.baseline_security_enabled ? 1 : 0
+  role       = aws_iam_role.config[0].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
 }
