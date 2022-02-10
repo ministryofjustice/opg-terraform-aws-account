4d1268e19b7c3d5f8c30fa91fbaefe4d89026ccf
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-02-10 14:16:39 +0000

    add disabled reason

--- modules/security_hub/cis_foundation_controls.tf
+++ modules/security_hub/cis_foundation_controls.tf
@@ -33,6 +33,7 @@ locals {
       standards_control_arn = "${local.cis_standard_controls_arn_path}/3.2"
       actions_enabled       = true
       control_status        = "DISABLED"
+      disabled_reason       = "See ADR https://docs.opg.service.justice.gov.uk/documentation/adrs/adr-004.html#adr-004-no-hardware-mfa-key-for-root-account"
       pattern               = "{($.eventName=\"ConsoleLogin\") && ($.additionalEventData.MFAUsed !=\"Yes\") && ($.userIdentity.sessionContext.sessionIssuer.arn != \"arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/eu-west-2/*\")}"
       alarm_description     = "console sign in without mfa count"
       alarm_threshold       = 1
