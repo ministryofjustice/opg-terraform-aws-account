d71ab5d047ae2435978e2fe9dc4af4851ac77b38
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-05-23 17:54:05 +0100

    remove conditional create

--- shield_advanced.tf
+++ shield_advanced.tf
@@ -1,5 +1,4 @@
 resource "aws_iam_role" "aws_srt_support" {
-  count              = var.shield_advanced_enabled ? 1 : 0
   name               = "AWSSRTSupport"
   description        = "Role for DDoS response team to review AWS resources in your account and to mitigate DDoS attacks against your infrastructure by creating WAF rules and AWS Shield protections."
   assume_role_policy = data.aws_iam_policy_document.aws_srt_support_assume_role_policy.json
@@ -20,7 +19,6 @@ data "aws_iam_policy_document" "aws_srt_support_assume_role_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "aws_srt_support_managed_policy" {
-  count      = var.shield_advanced_enabled ? 1 : 0
-  role       = aws_iam_role.aws_srt_support[0].name
+  role       = aws_iam_role.aws_srt_support.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSShieldDRTAccessPolicy"
 }
