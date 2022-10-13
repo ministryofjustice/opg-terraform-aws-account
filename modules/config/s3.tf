6e1cddefe002977e65d4d36c16c229723569b182
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-10-13 10:48:14 +0100

    formatting

--- modules/config/s3.tf
+++ modules/config/s3.tf
@@ -4,6 +4,7 @@ resource "aws_s3_bucket" "config" {
   bucket        = "config.${data.aws_region.current.name}.${var.account_name}.${var.product}.opg.justice.gov.uk"
   acl           = "private"
   force_destroy = true
+
   logging {
     target_bucket = var.s3_access_logging_bucket_name
     target_prefix = "log/config.${data.aws_region.current.name}.${var.account_name}.${var.product}.opg.justice.gov.uk/"
