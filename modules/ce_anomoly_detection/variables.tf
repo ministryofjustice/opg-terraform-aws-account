3566dca27c7b53b2b993526c52f1462d91639e72
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-10-27 12:20:09 +0100

    use sns feedback iam roles in all sns topics

--- modules/ce_anomoly_detection/variables.tf
+++ modules/ce_anomoly_detection/variables.tf
@@ -12,3 +12,13 @@ variable "immediate_schedule_threshold" {
   description = "The dollar value that triggers an immediate notification if the threshold is exceeded."
   type        = number
 }
+
+variable "sns_failure_feedback_role_arn" {
+  type        = string
+  description = "The ARN of the IAM role that allows Amazon SNS to write logs about SMS deliveries in CloudWatch Logs."
+}
+
+variable "sns_success_feedback_role_arn" {
+  type        = string
+  description = "The ARN of the IAM role that allows Amazon SNS to write logs about SMS deliveries in CloudWatch Logs."
+}
--- modules/config/variables.tf
+++ modules/config/variables.tf
@@ -34,3 +34,13 @@ variable "s3_access_logging_bucket_name" {
   description = "The name of the bucket that will receive the log objects"
   type        = string
 }
+
+variable "sns_failure_feedback_role_arn" {
+  type        = string
+  description = "The ARN of the IAM role that allows Amazon SNS to write logs about SMS deliveries in CloudWatch Logs."
+}
+
+variable "sns_success_feedback_role_arn" {
+  type        = string
+  description = "The ARN of the IAM role that allows Amazon SNS to write logs about SMS deliveries in CloudWatch Logs."
+}
