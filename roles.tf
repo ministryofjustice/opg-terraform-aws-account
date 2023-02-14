0723961ba8e1349c6f50ae3c1d368123661bd16b
Sam Wilkinson <sam.wilkinson@digital.justice.gov.uk>
2023-02-14 09:59:50 +0000

    Fix

--- roles.tf
+++ roles.tf
@@ -24,13 +24,13 @@ module "operator" {
 }
 
 module "breakglass" {
-  source                    = "./modules/default_roles"
-  name                      = "breakglass"
-  user_arns                 = var.user_arns.breakglass
-  base_policy_arn           = var.breakglass_base_policy_arn
-  custom_policy_json        = var.breakglass_custom_policy_json
-  create_instance_profile   = var.breakglass_create_instance_profile
-  create_assume_alarm_role  = true
+  source                  = "./modules/default_roles"
+  name                    = "breakglass"
+  user_arns               = var.user_arns.breakglass
+  base_policy_arn         = var.breakglass_base_policy_arn
+  custom_policy_json      = var.breakglass_custom_policy_json
+  create_instance_profile = var.breakglass_create_instance_profile
+  create_assume_alarm     = true
 }
 
 resource "aws_iam_role_policy_attachment" "aws_support_access_for_breakglass" {
