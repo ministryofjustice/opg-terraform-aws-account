9e4f5192d07be95398efca6aab1d1413d8e0ba3f
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-03-21 12:00:35 +0000

    move custom alarms to root from region

--- modules/region/variables.tf
+++ modules/region/variables.tf
@@ -27,14 +27,3 @@ variable "sns_success_feedback_role_arn" {
   type        = string
   description = "The ARN of the IAM role that allows Amazon SNS to write logs about SMS deliveries in CloudWatch Logs."
 }
-
-variable "custom_alarms_breakglass_login_alarm_enabled" {
-  default     = true
-  description = "Enable or disable the breakglass login alarm"
-  type        = bool
-}
-
-variable "cloudtrail_log_group_name" {
-  description = "Name of the cloudtrail log group"
-  type        = string
-}
