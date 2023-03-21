0ec08c23659265860686ad4100a6e3a33a2a302e
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-03-21 12:06:08 +0000

    remove unused output

--- modules/region/outputs.tf
+++ modules/region/outputs.tf
@@ -1,7 +1,3 @@
 output "access_logging_bucket" {
   value = aws_s3_bucket.s3_access_logging
 }
-
-output "aws_sns_topic_custom_cloudwatch_alarms" {
-  value = module.custom_cloudwatch_alarms.aws_sns_topic_custom_cloudwatch_alarms
-}
