1044b3b03d1869739a4863092450e8d357ee3a6b
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2023-08-23 10:10:57 +0100

    remove extra spaces (default editor config)

--- modules/security_hub/main.tf
+++ modules/security_hub/main.tf
@@ -1,7 +1,7 @@
 resource "aws_securityhub_account" "main" {
-    control_finding_generator = var.control_finding_generator
-    auto_enable_controls      = var.auto_enable_controls
-    enable_default_standards  = var.enable_default_standards
+  control_finding_generator = var.control_finding_generator
+  auto_enable_controls      = var.auto_enable_controls
+  enable_default_standards  = var.enable_default_standards
 }
 
 data "aws_caller_identity" "current" {
