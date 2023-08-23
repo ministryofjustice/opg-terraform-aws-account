a61a698ce284a9329e27d96f4416d59a4acddb92
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2023-08-23 11:09:13 +0100

    try to prevent destroy

--- modules/security_hub/main.tf
+++ modules/security_hub/main.tf
@@ -2,6 +2,10 @@ resource "aws_securityhub_account" "main" {
   control_finding_generator = var.control_finding_generator
   auto_enable_controls      = var.auto_enable_controls
   enable_default_standards  = var.enable_default_standards
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 data "aws_caller_identity" "current" {
