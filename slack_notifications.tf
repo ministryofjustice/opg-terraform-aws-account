babf5bea0e3ded7b90307dd17561e1afd2973932
Andy Price <sixdaysandy@gmail.com>
2023-10-10 15:01:55 +0100

    SP-1734 - Pull lambda versions from SSM to keep them upto date, use condition to prevent major version change #minor

--- slack_notifications.tf
+++ slack_notifications.tf
@@ -1,12 +1,12 @@
 module "aws_cost_notifier" {
   count              = local.aws_cost_anomaly_notifications_enabled ? 1 : 0
-  source             = "git@github.com:ministryofjustice/opg-aws-cost-notifier.git?ref=v1.2.0"
+  source             = "git@github.com:ministryofjustice/opg-aws-cost-notifier.git"
   account_name       = var.account_name
   ecr_repository_url = data.aws_ecr_repository.cost_notifier_lambda.repository_url
   slack_channel_id   = var.aws_slack_cost_anomaly_notification_channel
   slack_secret_arn   = aws_secretsmanager_secret.aws_notifier_slack_token[0].arn
   sns_topic_arn      = module.cost_anomaly_detection.immediate_schedule_sns_topic.arn
-  version_tag        = "v1.2.0"
+  version_tag        = data.aws_ssm_parameter.cost_notifier_lambda_version.value
   providers = {
     aws           = aws
     aws.us-east-1 = aws.global
@@ -15,12 +15,12 @@ module "aws_cost_notifier" {
 
 module "aws_health_notifier" {
   count              = local.aws_health_notifications_enabled ? 1 : 0
-  source             = "git@github.com:ministryofjustice/opg-aws-health-notifier.git?ref=v1.2.0"
+  source             = "git@github.com:ministryofjustice/opg-aws-health-notifier.git"
   account_name       = var.account_name
   ecr_repository_url = data.aws_ecr_repository.health_notifier_lambda.repository_url
   slack_channel_id   = var.aws_slack_health_notification_channel
   slack_secret_arn   = aws_secretsmanager_secret.aws_notifier_slack_token[0].arn
-  version_tag        = "v1.2.0"
+  version_tag        = data.aws_ssm_parameter.health_notifier_lambda_version.value
   providers = {
     aws           = aws
     aws.eu-west-2 = aws.eu-west-2
@@ -36,6 +36,30 @@ data "aws_ecr_repository" "cost_notifier_lambda" {
 data "aws_ecr_repository" "health_notifier_lambda" {
   name     = "shared/aws-health-notifier"
   provider = aws.management
+}
+
+data "aws_ssm_parameter" "cost_notifier_lambda_version" {
+  name     = "/shared/aws-cost-notifier-lambda-version"
+  provider = aws.management
+
+  lifecycle {
+    postcondition {
+      condition     = can(regex("^[v][1][.]", self.value))
+      error_message = "Cost Notifier Version Must Be v1.x.x"
+    }
+  }
+}
+
+data "aws_ssm_parameter" "health_notifier_lambda_version" {
+  name     = "/shared/aws-health-notifier-lambda-version"
+  provider = aws.management
+
+  lifecycle {
+    postcondition {
+      condition     = can(regex("^[v][1][.]", self.value))
+      error_message = "Healthgitst Notifier Version Must Be v1.x.x"
+    }
+  }
 }
 
 
