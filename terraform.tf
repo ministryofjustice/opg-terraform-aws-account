4f93f18f0ae14a4b33b8922063e059f74dfcf6f4
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-01-08 14:48:41 +0000

    split out versions info as we do normally #patch update workflow to use shared actions

--- terraform.tf
+++ terraform.tf
@@ -1,16 +1,1 @@
-terraform {
-  required_version = ">= 1.1.5"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-      configuration_aliases = [
-        aws.eu-west-2,
-        aws.global,
-        aws.management,
-      ]
-    }
-  }
-}
-
 data "aws_region" "current" {}
