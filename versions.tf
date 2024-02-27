73c07b4b8a6bcd95732d7cd9a4bdde9fe2d4b1b6
Andy Price <sixdaysandy@gmail.com>
2024-02-27 09:16:27 +0000

    SP-1923 - Increase version constraint on module, and region module #patch (#78)

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.38"
       configuration_aliases = [
         aws.eu-west-2,
         aws.global,
