facdf1554b65748491a429c5514d18ca29eeef3b
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-05-03 12:17:59 +0100

    add kms key for sns

--- modules/security_hub/findings_notifications.tf
+++ modules/security_hub/findings_notifications.tf
@@ -33,7 +33,7 @@ resource "aws_sns_topic" "security_hub" {
   count             = local.security_hub_pagerduty_integration_enabled ? 1 : 0
   name              = "SecurityHub-to-PagerDuty-${var.account_name}"
   fifo_topic        = false
-  kms_master_key_id = "alias/aws/sns"
+  kms_master_key_id = aws_kms_key.security_hub_sns.arn
 }
 
 resource "aws_sns_topic_subscription" "security_hub" {
@@ -62,3 +62,66 @@ data "aws_iam_policy_document" "security_hub_sns_topic" {
     resources = [aws_sns_topic.security_hub[0].arn]
   }
 }
+
+resource "aws_kms_key" "security_hub_sns" {
+  description = "KMS Key for Security Hub to SNS"
+  policy      = data.aws_iam_policy_document.security_hub_sns_key.json
+}
+
+data "aws_iam_policy_document" "security_hub_sns_key" {
+  statement {
+    sid       = "Enable IAM User Permissions"
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["kms:*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+
+  statement {
+    sid    = "AllowEventBridgeDecrypt"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*",
+    ]
+    resources = [aws_sns_topic.security_hub[0].arn]
+  }
+
+  statement {
+    sid       = "Allow access for Key Administrators"
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ci"]
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
