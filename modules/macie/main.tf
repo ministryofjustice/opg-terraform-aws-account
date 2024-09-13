e4cda971aa0d2f5f6c4f5a64bee338eaa11b988d
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-09-13 20:20:27 +0100

    use global encryption key

--- modules/macie/main.tf
+++ modules/macie/main.tf
@@ -10,7 +10,7 @@ resource "aws_macie2_classification_export_configuration" "main" {
   ]
   s3_destination {
     bucket_name = aws_s3_bucket.bucket.bucket
-    kms_key_arn = module.macie_findings.eu_west_1_kms_key_arn
+    kms_key_arn = aws_kms_alias.macie_findings_encryption_key_alias_global.target_key_arn
   }
   provider = aws.global
 }
