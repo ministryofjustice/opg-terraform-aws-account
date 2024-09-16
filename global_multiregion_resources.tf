b410d2d8f7ed4584d9ec82a3310d37dc53b894ae
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-09-16 13:30:40 +0100

    fix references to providers

--- global_multiregion_resources.tf
+++ global_multiregion_resources.tf
@@ -1,7 +1,7 @@
 module "global_multiregion_resources" {
   source = "./modules/global"
   providers = {
-    aws.eu_west_1 = aws.eu_west_1
+    aws.eu_west_1 = aws
     aws.eu_west_2 = aws.eu_west_2
     aws.global    = aws.global
   }
