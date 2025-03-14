45e00381f0afd95f108401516cf2d86a1cfe4f7a
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-03-14 10:26:03 +0000

    add service-linked role for config

--- config_iam.tf
+++ config_iam.tf
@@ -1,7 +1,13 @@
-resource "aws_iam_role" "config" {
-  count              = local.config_enabled ? 1 : 0
-  name               = "aws-config-${var.product}-${var.account_name}"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+# resource "aws_iam_role" "config" {
+#   count              = local.config_enabled ? 1 : 0
+#   name               = "aws-config-${var.product}-${var.account_name}"
+#   path               = "/service-role/"
+#   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+# }
+
+resource "aws_iam_service_linked_role" "config" {
+  count            = local.config_enabled ? 1 : 0
+  aws_service_name = "config.amazonaws.com"
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
@@ -20,6 +26,6 @@ data "aws_iam_policy_document" "assume_role_policy" {
 
 resource "aws_iam_role_policy_attachment" "managed_policy" {
   count      = local.config_enabled ? 1 : 0
-  role       = aws_iam_role.config[0].name
+  role       = aws_iam_service_linked_role.config[0].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWS_ConfigRole"
 }
