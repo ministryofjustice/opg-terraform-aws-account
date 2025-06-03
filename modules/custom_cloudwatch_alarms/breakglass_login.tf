fdfe1be5a17015e27b024d601df9558dc3fa5d2a
Andy Price <sixdaysandy@gmail.com>
2025-06-03 09:15:41 +0100

    SP-2857 - Don't auto resolve alarms for sensitive role access #minor (#108)

--- modules/custom_cloudwatch_alarms/breakglass_login.tf
+++ modules/custom_cloudwatch_alarms/breakglass_login.tf
@@ -16,7 +16,6 @@ resource "aws_cloudwatch_metric_alarm" "breakglass_login_alarm" {
   actions_enabled     = true
   alarm_name          = "${var.account_name} breakglass console login check"
   alarm_actions       = [aws_sns_topic.custom_cloudwatch_alarms.arn]
-  ok_actions          = [aws_sns_topic.custom_cloudwatch_alarms.arn]
   alarm_description   = "number of breakglass logins"
   namespace           = "${var.product}/Cloudtrail"
   metric_name         = "BreakglassLoginCount"
