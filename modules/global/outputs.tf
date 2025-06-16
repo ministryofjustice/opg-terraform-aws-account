140f346fd2708621ae096192122983d32c28afb6
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-06-16 09:55:04 +0100

    values for outputs are a tuple

--- modules/global/outputs.tf
+++ modules/global/outputs.tf
@@ -1,7 +1,7 @@
 output "eu_west_1_macie_findings_encryption_key" {
-  value = var.macie_enabled ? module.macie_findings_encryption_key.eu_west_1_target_key : null
+  value = var.macie_enabled ? module.macie_findings_encryption_key[0].eu_west_1_target_key : null
 }
 
 output "eu_west_2_macie_findings_encryption_key" {
-  value = var.macie_enabled ? module.macie_findings_encryption_key.eu_west_2_target_key : null
+  value = var.macie_enabled ? module.macie_findings_encryption_key[0].eu_west_2_target_key : null
 }
