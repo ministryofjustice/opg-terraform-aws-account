e971e5f4ffd432b9bfa12b9c7488bb392b1de331
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-08-06 10:48:56 +0100

    rename resource

--- modules/security_hub/aws_resource_tagging_standard.tf
+++ modules/security_hub/aws_resource_tagging_standard.tf
@@ -1,4 +1,4 @@
-resource "aws_securityhub_standards_subscription" "cis" {
+resource "aws_securityhub_standards_subscription" "resource_tagging" {
   depends_on    = [aws_securityhub_account.main]
   standards_arn = "arn:aws:securityhub:${data.aws_region.current.name}::standards/aws-resource-tagging-standard/v/1.0.0"
 }
