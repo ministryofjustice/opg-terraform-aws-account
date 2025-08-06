5e7c726c7566be9f1037c34548120598d90c1acc
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-08-06 11:02:23 +0100

    increase timeout for creates and deletes

--- modules/security_hub/aws_resource_tagging_standard.tf
+++ modules/security_hub/aws_resource_tagging_standard.tf
@@ -1,4 +1,9 @@
 resource "aws_securityhub_standards_subscription" "resource_tagging" {
   depends_on    = [aws_securityhub_account.main]
   standards_arn = "arn:aws:securityhub:${data.aws_region.current.name}::standards/aws-resource-tagging-standard/v/1.0.0"
+  timeouts {
+    create = "10m"
+    delete = "10m"
+  }
+
 }
