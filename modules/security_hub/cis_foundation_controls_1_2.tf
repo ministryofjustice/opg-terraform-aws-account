109f046ba925673ceb4da22ed5e3807bdb25cf2d
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-08-19 13:02:40 +0100

    rename cis control vars to be for 1.2.0

--- modules/security_hub/cis_foundation_controls_1_2.tf
+++ modules/security_hub/cis_foundation_controls_1_2.tf
@@ -6,15 +6,15 @@ resource "aws_securityhub_standards_subscription" "cis_1_2" {
 
 resource "aws_securityhub_standards_control" "cis_1_14_ensure_hardware_mfa_is_enabled_for_the_root_account" {
   standards_control_arn = "${local.cis_standard_controls_arn_path}/1.14"
-  control_status        = var.cis_foundation_control_1_14_enabled ? "ENABLED" : "DISABLED"
-  disabled_reason       = var.cis_foundation_control_1_14_enabled ? null : "See ADR https://docs.opg.service.justice.gov.uk/documentation/adrs/adr-004.html#adr-004-no-hardware-mfa-key-for-root-account"
+  control_status        = var.cis_1_2_foundation_control_1_14_enabled ? "ENABLED" : "DISABLED"
+  disabled_reason       = var.cis_1_2_foundation_control_1_14_enabled ? null : "See ADR https://docs.opg.service.justice.gov.uk/documentation/adrs/adr-004.html#adr-004-no-hardware-mfa-key-for-root-account"
   depends_on = [
     aws_securityhub_account.main
   ]
 }
 locals {
   cis_standard_controls_arn_path             = "arn:aws:securityhub:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:control/cis-aws-foundations-benchmark/v/1.2.0"
-  cis_foundation_control_3_10_custom_enabled = var.cis_foundation_control_3_10_custom_filter == "" ? false : true
+  cis_foundation_control_3_10_custom_enabled = var.cis_1_2_foundation_control_3_10_custom_filter == "" ? false : true
   cis_controls = {
     cis_3_1_unauthorised_api_calls = {
       metric_name           = "CIS-3.1-UnauthorizedAPICalls"
@@ -37,8 +37,8 @@ locals {
     cis_3_4_iam_policy_changes = {
       metric_name           = "CIS-3.4-IAMPolicyChanges"
       standards_control_arn = "${local.cis_standard_controls_arn_path}/3.4"
-      actions_enabled       = var.cis_foundation_control_3_4_enabled
-      control_status        = var.cis_foundation_control_3_4_enabled ? "ENABLED" : "DISABLED"
+      actions_enabled       = var.cis_1_2_foundation_control_3_4_enabled
+      control_status        = var.cis_1_2_foundation_control_3_4_enabled ? "ENABLED" : "DISABLED"
       pattern               = "{($.eventName=DeleteGroupPolicy) || ($.eventName=DeleteRolePolicy) || ($.eventName=DeleteUserPolicy) || ($.eventName=PutGroupPolicy) || ($.eventName=PutRolePolicy) || ($.eventName=PutUserPolicy) || ($.eventName=CreatePolicy) || ($.eventName=DeletePolicy) || ($.eventName=CreatePolicyVersion) || ($.eventName=DeletePolicyVersion) || ($.eventName=AttachRolePolicy) || ($.eventName=DetachRolePolicy) || ($.eventName=AttachUserPolicy) || ($.eventName=DetachUserPolicy) || ($.eventName=AttachGroupPolicy) || ($.eventName=DetachGroupPolicy)}"
       alarm_description     = "iam policy changes count"
       alarm_threshold       = 1
@@ -73,8 +73,8 @@ locals {
     cis_3_8_s3_bucket_policy_changes = {
       metric_name           = "CIS-3.8-S3BucketPolicyChanges"
       standards_control_arn = "${local.cis_standard_controls_arn_path}/3.8"
-      actions_enabled       = var.cis_foundation_control_3_8_enabled
-      control_status        = var.cis_foundation_control_3_8_enabled ? "ENABLED" : "DISABLED"
+      actions_enabled       = var.cis_1_2_foundation_control_3_8_enabled
+      control_status        = var.cis_1_2_foundation_control_3_8_enabled ? "ENABLED" : "DISABLED"
       pattern               = "{($.eventSource=s3.amazonaws.com) && (($.eventName=PutBucketAcl) || ($.eventName=PutBucketPolicy) || ($.eventName=PutBucketCors) || ($.eventName=PutBucketLifecycle) || ($.eventName=PutBucketReplication) || ($.eventName=DeleteBucketPolicy) || ($.eventName=DeleteBucketCors) || ($.eventName=DeleteBucketLifecycle) || ($.eventName=DeleteBucketReplication))}"
       alarm_description     = "s3 bucket policy changes count"
       alarm_threshold       = 1
@@ -91,17 +91,17 @@ locals {
     cis_3_10_security_group_changes = {
       metric_name           = "CIS-3.10-SecurityGroupChanges"
       standards_control_arn = "${local.cis_standard_controls_arn_path}/3.10"
-      actions_enabled       = var.cis_foundation_control_3_10_enabled || local.cis_foundation_control_3_10_custom_enabled ? true : false
-      control_status        = var.cis_foundation_control_3_10_enabled && !local.cis_foundation_control_3_10_custom_enabled ? "ENABLED" : "DISABLED"
-      pattern               = local.cis_foundation_control_3_10_custom_enabled ? var.cis_foundation_control_3_10_custom_filter : "{($.eventName=AuthorizeSecurityGroupIngress) || ($.eventName=AuthorizeSecurityGroupEgress) || ($.eventName=RevokeSecurityGroupIngress) || ($.eventName=RevokeSecurityGroupEgress) || ($.eventName=CreateSecurityGroup) || ($.eventName=DeleteSecurityGroup)}"
+      actions_enabled       = var.cis_1_2_foundation_control_3_10_enabled || local.cis_foundation_control_3_10_custom_enabled ? true : false
+      control_status        = var.cis_1_2_foundation_control_3_10_enabled && !local.cis_foundation_control_3_10_custom_enabled ? "ENABLED" : "DISABLED"
+      pattern               = local.cis_foundation_control_3_10_custom_enabled ? var.cis_1_2_foundation_control_3_10_custom_filter : "{($.eventName=AuthorizeSecurityGroupIngress) || ($.eventName=AuthorizeSecurityGroupEgress) || ($.eventName=RevokeSecurityGroupIngress) || ($.eventName=RevokeSecurityGroupEgress) || ($.eventName=CreateSecurityGroup) || ($.eventName=DeleteSecurityGroup)}"
       alarm_description     = "aws config configuration changes count"
       alarm_threshold       = 1
     }
     cis_3_11_changes_to_network_access_control_lists = {
       metric_name           = "CIS-3.11-NetworkACLChanges"
       standards_control_arn = "${local.cis_standard_controls_arn_path}/3.11"
-      actions_enabled       = var.cis_foundation_control_3_11_enabled
-      control_status        = var.cis_foundation_control_3_11_enabled ? "ENABLED" : "DISABLED"
+      actions_enabled       = var.cis_1_2_foundation_control_3_11_enabled
+      control_status        = var.cis_1_2_foundation_control_3_11_enabled ? "ENABLED" : "DISABLED"
       pattern               = "{($.eventName=CreateNetworkAcl) || ($.eventName=CreateNetworkAclEntry) || ($.eventName=DeleteNetworkAcl) || ($.eventName=DeleteNetworkAclEntry) || ($.eventName=ReplaceNetworkAclEntry) || ($.eventName=ReplaceNetworkAclAssociation)}"
       alarm_description     = "aws config configuration changes count"
       alarm_threshold       = 1
@@ -109,8 +109,8 @@ locals {
     cis_3_12_changes_to_network_gateways = {
       metric_name           = "CIS-3.12-NetworkGatewayChanges"
       standards_control_arn = "${local.cis_standard_controls_arn_path}/3.12"
-      actions_enabled       = var.cis_foundation_control_3_12_enabled
-      control_status        = var.cis_foundation_control_3_12_enabled ? "ENABLED" : "DISABLED"
+      actions_enabled       = var.cis_1_2_foundation_control_3_12_enabled
+      control_status        = var.cis_1_2_foundation_control_3_12_enabled ? "ENABLED" : "DISABLED"
       pattern               = "{($.eventName=CreateCustomerGateway) || ($.eventName=DeleteCustomerGateway) || ($.eventName=AttachInternetGateway) || ($.eventName=CreateInternetGateway) || ($.eventName=DeleteInternetGateway) || ($.eventName=DetachInternetGateway)}"
       alarm_description     = "aws config configuration changes count"
       alarm_threshold       = 1
@@ -118,8 +118,8 @@ locals {
     cis_3_13_changes_to_route_tables = {
       metric_name           = "CIS-3.13-RouteTableChanges"
       standards_control_arn = "${local.cis_standard_controls_arn_path}/3.13"
-      actions_enabled       = var.cis_foundation_control_3_13_enabled
-      control_status        = var.cis_foundation_control_3_13_enabled ? "ENABLED" : "DISABLED"
+      actions_enabled       = var.cis_1_2_foundation_control_3_13_enabled
+      control_status        = var.cis_1_2_foundation_control_3_13_enabled ? "ENABLED" : "DISABLED"
       pattern               = "{($.eventName=CreateRoute) || ($.eventName=CreateRouteTable) || ($.eventName=ReplaceRoute) || ($.eventName=ReplaceRouteTableAssociation) || ($.eventName=DeleteRouteTable) || ($.eventName=DeleteRoute) || ($.eventName=DisassociateRouteTable)}"
       alarm_description     = "aws config configuration changes count"
       alarm_threshold       = 1
@@ -127,8 +127,8 @@ locals {
     cis_3_14_vpc_changes = {
       metric_name           = "CIS-3.14-VPCChanges"
       standards_control_arn = "${local.cis_standard_controls_arn_path}/3.14"
-      actions_enabled       = var.cis_foundation_control_3_14_enabled
-      control_status        = var.cis_foundation_control_3_14_enabled ? "ENABLED" : "DISABLED"
+      actions_enabled       = var.cis_1_2_foundation_control_3_14_enabled
+      control_status        = var.cis_1_2_foundation_control_3_14_enabled ? "ENABLED" : "DISABLED"
       pattern               = "{($.eventName=CreateVpc) || ($.eventName=DeleteVpc) || ($.eventName=ModifyVpcAttribute) || ($.eventName=AcceptVpcPeeringConnection) || ($.eventName=CreateVpcPeeringConnection) || ($.eventName=DeleteVpcPeeringConnection) || ($.eventName=RejectVpcPeeringConnection) || ($.eventName=AttachClassicLinkVpc) || ($.eventName=DetachClassicLinkVpc) || ($.eventName=DisableVpcClassicLink) || ($.eventName=EnableVpcClassicLink)}"
       alarm_description     = "vpc changes count"
       alarm_threshold       = 1
--- modules/security_hub/variables.tf
+++ modules/security_hub/variables.tf
@@ -19,41 +19,41 @@ variable "aws_cloudwatch_log_group_cloudtrail_name" {
   description = "Name of the trail."
 }
 
-variable "cis_foundation_control_1_14_enabled" {
+variable "cis_1_2_foundation_control_1_14_enabled" {
   type        = bool
   description = "When true, creates a metric filter and alarm for CIS.1.14. When false, sets standard control to disabled."
 }
-variable "cis_foundation_control_3_4_enabled" {
+variable "cis_1_2_foundation_control_3_4_enabled" {
   type        = bool
   description = "When true, creates a metric filter and alarm for CIS.3.4. When false, sets standard control to disabled."
 }
-variable "cis_foundation_control_3_8_enabled" {
+variable "cis_1_2_foundation_control_3_8_enabled" {
   type        = bool
   description = "When true, creates a metric filter and alarm for CIS.3.8. When false, sets standard control to disabled."
 }
-variable "cis_foundation_control_3_10_enabled" {
+variable "cis_1_2_foundation_control_3_10_enabled" {
   type        = bool
   description = "When true, creates a metric filter and alarm for CIS.3.10. When false, sets standard control to disabled."
 }
-variable "cis_foundation_control_3_10_custom_filter" {
+variable "cis_1_2_foundation_control_3_10_custom_filter" {
   type        = string
   default     = ""
   description = "When provided, creates a custom metric filter and alarm for CIS.3.10 and disables the control."
 }
-variable "cis_foundation_control_3_11_enabled" {
+variable "cis_1_2_foundation_control_3_11_enabled" {
   type        = bool
   description = "When true, creates a metric filter and alarm for CIS.3.11. When false, sets standard control to disabled."
 }
-variable "cis_foundation_control_3_12_enabled" {
+variable "cis_1_2_foundation_control_3_12_enabled" {
   type        = bool
   description = "When true, creates a metric filter and alarm for CIS.3.12. When false, sets standard control to disabled."
 }
-variable "cis_foundation_control_3_13_enabled" {
+variable "cis_1_2_foundation_control_3_13_enabled" {
   type        = bool
   description = "When true, creates a metric filter and alarm for CIS.3.13. When false, sets standard control to disabled."
 }
 
-variable "cis_foundation_control_3_14_enabled" {
+variable "cis_1_2_foundation_control_3_14_enabled" {
   type        = bool
   description = "When true, creates a metric filter and alarm for CIS.3.14. When false, sets standard control to disabled."
 }
