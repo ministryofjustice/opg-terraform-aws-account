8b1a947111acf41806ac5f198f7e8f9cabd778b2
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-11-26 09:21:29 +0000

    replace ACL with bucketOwnerEnforced ownership controls

--- modules/config/s3.tf
+++ modules/config/s3.tf
@@ -12,9 +12,12 @@ resource "aws_s3_bucket_versioning" "config" {
   }
 }
 
-resource "aws_s3_bucket_acl" "config" {
+resource "aws_s3_bucket_ownership_controls" "config" {
   bucket = aws_s3_bucket.config.id
-  acl    = "private"
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
 }
 
 resource "aws_s3_bucket_logging" "config" {
