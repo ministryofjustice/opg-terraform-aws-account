ce4aa4113c22cfb3ce0a8d6b2fe5ce375ace5a0e
AidanJ97 <aidan.johnson@digital.justice.gov.uk>
2026-06-10 12:37:45 +0100

    SP-3846 - default to medium alerting #minor

--- modules/guardduty/cloudwatch.tf
+++ modules/guardduty/cloudwatch.tf
@@ -44,7 +44,7 @@ resource "aws_cloudwatch_event_target" "guardduty_findings_log_group" {
 
 resource "aws_cloudwatch_log_metric_filter" "guardduty_findings" {
   count          = local.alerting_enabled ? 1 : 0
-  name           = "guardduty-findings-${var.alert_minimum_severity}-and-above"
+  name           = "guardduty-findings"
   pattern        = "{ $.detail.severity >= ${local.alert_threshold} }"
   log_group_name = aws_cloudwatch_log_group.guardduty_findings[0].name
 
@@ -57,15 +57,16 @@ resource "aws_cloudwatch_log_metric_filter" "guardduty_findings" {
 
 resource "aws_cloudwatch_metric_alarm" "guardduty_findings" {
   count               = local.alerting_enabled ? 1 : 0
-  alarm_name          = "guardduty-findings-${var.alert_minimum_severity}-and-above"
+  alarm_name          = "guardduty-findings"
   alarm_description   = "GuardDuty finding at or above ${var.alert_minimum_severity} severity detected"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
+  datapoints_to_alarm = 1
   metric_name         = "GuardDutyFindingCount"
   namespace           = "GuardDuty"
-  period              = 300
+  period              = 60
   statistic           = "Sum"
   threshold           = 1
-  treat_missing_data  = "ignore"
+  treat_missing_data  = "notBreaching"
   alarm_actions       = [aws_sns_topic.guardduty_findings[0].arn]
 }
