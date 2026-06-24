34c1ea617ceeab555241544edbbe146208862c0a
Andy Price <sixdaysandy@gmail.com>
2026-06-24 15:11:08 +0100

    SP-3884 - Disable ELB & VPC Security Hub Controls that we either cannnot, or shoose not to comply with #minor

--- modules/region/modules/security_hub_controls/aws_foundational_security_best_practices.tf
+++ modules/region/modules/security_hub_controls/aws_foundational_security_best_practices.tf
@@ -2,10 +2,28 @@ locals {
   fsbp_standard_controls_arn_path = "arn:aws:securityhub:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:control/aws-foundational-security-best-practices/v/1.0.0"
 }
 
+resource "aws_securityhub_standards_control" "ec2_vpc_172_vpc_bpa_should_be_enabled" {
+  standards_control_arn = "${local.fsbp_standard_controls_arn_path}/EC2.172"
+  control_status        = "DISABLED"
+  disabled_reason       = "We cannot enable VPC Block Public Access with Network Firewall"
+}
+
 resource "aws_securityhub_standards_control" "elb_6_application_load_balancer_deletion_protection_should_be_enabled" {
   standards_control_arn = "${local.fsbp_standard_controls_arn_path}/ELB.6"
   control_status        = var.security_hub_config.fsbp_standard_control_elb_6_enabled ? "ENABLED" : "DISABLED"
   disabled_reason       = var.security_hub_config.fsbp_standard_control_elb_6_enabled ? null : "Non-production accounts contain ephemeral environments."
+}
+
+resource "aws_securityhub_standards_control" "elb_22_target_group_healthchecks_should_use_tls_enabled" {
+  standards_control_arn = "${local.fsbp_standard_controls_arn_path}/ELB.21"
+  control_status        = var.security_hub_config.fsbp_standard_control_elb_21_enabled ? "ENABLED" : "DISABLED"
+  disabled_reason       = var.security_hub_config.fsbp_standard_control_elb_21_enabled ? null : "We choose to terminate SSL ath the Load Balancer"
+}
+
+resource "aws_securityhub_standards_control" "elb_22_target_groups_should_use_tls_enabled" {
+  standards_control_arn = "${local.fsbp_standard_controls_arn_path}/ELB.22"
+  control_status        = var.security_hub_config.fsbp_standard_control_elb_22_enabled ? "ENABLED" : "DISABLED"
+  disabled_reason       = var.security_hub_config.fsbp_standard_control_elb_22_enabled ? null : "We choose to terminate SSL ath the Load Balancer"
 }
 
 resource "aws_securityhub_standards_control" "iam_6_hardware_mfa_should_be_enabled_for_the_root_user" {
